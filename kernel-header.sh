4f875cfe852dcc3df044aab0f8bb119dbaa3c5a1
root <root@NPN-2.adlinktech.com>
2021-02-04 07:32:25 +0000

    Support different BSP versions

--- kernel-header.sh
+++ kernel-header.sh
@@ -5,6 +5,9 @@ apt update
 apt install -y bc
 cd /lib/modules/$(uname -r)/source
 make oldconfig # generate .config if it's not existed
-sed -i 's/CONFIG_LOCALVERSION="-v1.0.6"$/CONFIG_LOCALVERSION="-v1.0.6+"/g' .config
+BSP_VERSION=$(echo $(uname -r) | cut -d"-" -f2)
+if [ ${BSP_VERSION: -1} == '+' ]; then
+    sed -i "s/CONFIG_LOCALVERSION=\"-${BSP_VERSION:0:-1}\"$/CONFIG_LOCALVERSION=\"-$BSP_VERSION\"/g" .config
+fi
 make prepare && make prepare scripts
 
